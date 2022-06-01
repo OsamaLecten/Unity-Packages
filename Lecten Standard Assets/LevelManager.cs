fe84c78fce40ea232c4d2bb7f8ed6fccef98427c
OsamaLecten <osama.hamada@lecten.com>
2022-06-01 12:17:25 +0300

    Updated Level Manager to the new version

--- Lecten Standard Assets/LevelManager.cs
+++ Lecten Standard Assets/LevelManager.cs
@@ -1,28 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
+using StandardAssets;
+using UnityEngine.UI;
+
 
 public class LevelManager : MonoBehaviour
 {
-    public static LevelManager scr;
     public int levelCount;
     public int[] tutorialLevels;
-    public GameObject openThisLevel;
-    public static GameObject currentLevel;
     [Space(16)]
-    public bool spawn;
-
-    [HideInInspector]
+    public bool Testing;
     public List<int> lvl;
-    bool allLevelsDone;
-    private void Awake()
-    {
-        scr = this;
-        SetValues();
-        
-
-    }
+    public GameObject TestLevel;
+    public Text levelText;
     private void Start()
     {
+        SetValues();
         SpawnLevel();
     }
     private void SetValues()
@@ -31,39 +24,28 @@ public class LevelManager : MonoBehaviour
         {
             lvl.Add(i + 1);
         }
-        if ((StandardAssets.SA.GetLevel() + 1) > lvl.Count)
+        if ((SA.SaveData.level + 1) >= lvl.Count)
         {
             for (int i = 0; i < tutorialLevels.Length; i++)
             {
                 lvl.Remove(tutorialLevels[i]);
             }
-            allLevelsDone = true;
         }
+        levelText.text = "Level " + (SA.SaveData.level + 1);
     }
-    GameObject obj;
     private void SpawnLevel()
     {
-        if (spawn)
+        if (!Testing)
         {
-            if (openThisLevel != null)
-            {
-                obj = openThisLevel;
-            }
-            else
-            {
-                if (!allLevelsDone)
-                {
-                    obj = UnityEngine.Resources.Load<GameObject>("Levels/Level " + lvl[StandardAssets.SA.GetLevel() % lvl.Count]);
-                }
-                else
-                {
-                    obj = UnityEngine.Resources.Load<GameObject>("Levels/Level " + lvl[(StandardAssets.SA.GetLevel()-tutorialLevels.Length) % lvl.Count]);
-                }
-                
-            }
-
-            currentLevel = Instantiate(obj);
+            int index = lvl[SA.SaveData.level % lvl.Count];
+            GameObject obj = Resources.Load<GameObject>("Level/Level " + index);
+            Instantiate(obj);
+        }
+        else
+        {
+            Debug.LogWarning("Test level loaded.");
+            GameObject obj = TestLevel;
+            Instantiate(obj);
         }
     }
-
 }
